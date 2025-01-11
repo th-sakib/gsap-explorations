a84135da30e1475af721ee9bc212dfc91a5dcb75
th-sakib <mdthamidulhasansakib@gmail.com>
2025-01-12 00:39:06 +0600

    centerred the div and calculated x and y of the child div

--- dancing rope/script.js
+++ dancing rope/script.js
@@ -5,7 +5,10 @@ let finalPath = `M 10 100 Q 500 100 990 100`;
 let string = document.querySelector("#string");
 
 string.addEventListener("mousemove", function (point) {
-  path = `M 10 100 Q 500 ${point.y} 990 100`;
+  let rect = string.getBoundingClientRect(); // Get bounding box of the div
+  let x = point.clientX - rect.left; // Relative x-coordinate
+  let y = point.clientY - rect.top; // Relative y-coordinate
+  let path = `M 10 100 Q ${x} ${y} 990 100`;
   gsap.to("svg path", {
     attr: { d: path },
     duration: 1,
